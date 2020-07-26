7cf48a4782914f307e0b105244eb25fa8ff62cd9
robings <58939401+robings@users.noreply.github.com>
2020-07-26 12:46:04 +0100

    Enabled editing of current balance

--- bankOfMumAndDad/Requests/PutRequest.cs
+++ bankOfMumAndDad/Requests/PutRequest.cs
@@ -6,5 +6,6 @@ namespace bankOfMumAndDad.Requests
         public long Id { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
+        public decimal? CurrentBalance { get; set; }
     }
 }
