89da50a9b7f44ff8a5c25bef41cdaf329fce6c9b
robings <58939401+robings@users.noreply.github.com>
2020-09-20 16:31:36 +0100

    Add table to hold accounts.

--- bank-of-m-and-d-fe/src/Components/AccountsList/AccountsList.js
+++ bank-of-m-and-d-fe/src/Components/AccountsList/AccountsList.js
@@ -1,10 +1,37 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './accountsList.css';
 
 function AccountsList() {
+    const [data, setData] = useState([]);
+
+    async function getAllAccounts() {
+        const response = await fetch('http://localhost:55741/api/Account/all');
+        const json = await response.json();
+
+        setData(json.data);
+    }
+
+    useEffect(() => {
+        getAllAccounts();
+    }, []);
+
     return (
         <main>
             <h2>Accounts</h2>
+            <table>
+                <thead>
+                    <tr><th>Last Name</th><th>FirstName</th><th>Current Balance</th></tr>
+                </thead>
+                <tbody>
+                    {data.map(({ id, firstName, lastName, currentBalance }) => (
+                        <tr className='data' key={id}>
+                            <td>{lastName}</td>
+                            <td>{firstName}</td>
+                            <td>£{currentBalance}</td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
         </main>
     );
 }
