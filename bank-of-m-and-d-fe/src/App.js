c05f9bb7eccb3a09c39393ef276e241982bf643a
robings <58939401+robings@users.noreply.github.com>
2020-09-20 22:54:28 +0100

    Completed transactions component to allow passing in of props.

--- bank-of-m-and-d-fe/src/App.js
+++ bank-of-m-and-d-fe/src/App.js
@@ -4,12 +4,20 @@ import AccountsList from './Components/AccountsList/AccountsList';
 import Transactions from './Components/Transactions/Transactions'
 import './App.css';
 
+const accountData = {
+  id: 10024,
+  firstName: "Jemima",
+  lastName: "Canard",
+  openingBalance: 300,
+  currentBalance: 300,
+};
+
 function App() {
   return (
     <div className="App">
       <Header />
       <AccountsList />
-      <Transactions />
+      <Transactions accountData={accountData} />
     </div>
   );
 }
