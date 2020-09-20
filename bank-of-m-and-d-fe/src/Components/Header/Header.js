ab1817b77a73dad1a334c24d6c5a96fceb200475
robings <58939401+robings@users.noreply.github.com>
2020-09-20 12:10:23 +0100

    Added Header component.

---
+++ bank-of-m-and-d-fe/src/Components/Header/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './header.css';
+
+function Header() {
+    return (
+        <header>
+            <div>
+                Bank Of Mum And Dad
+          </div>
+        </header>
+    );
+}
+
+export default Header;
