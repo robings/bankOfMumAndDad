57eb29a7869d4a3f1051092f52a508ffc02bea29
robings <58939401+robings@users.noreply.github.com>
2020-11-07 13:34:27 +0000

    Move login/logout buttons to header component.

--- bank-of-m-and-d-fe/src/Components/AccountsNav/AccountsNav.js
+++ bank-of-m-and-d-fe/src/Components/AccountsNav/AccountsNav.js
@@ -1,21 +1,9 @@
 import React from 'react';
-import { toast } from 'react-toastify';
 import './AccountsNav.css';
 
 function AccountsNav(props) {
-    function home() {
-        window.location.reload();
-      }
-    const handleLogoutClick = () => {
-        localStorage.removeItem('bearerToken');
-        toast.success("You have logged out.");
-        setTimeout(home, 5000);
-    }
-
     return (
             <div className="subNav">
-                {localStorage.getItem('bearerToken') && (<button className="subNavButton" onClick={handleLogoutClick}>Logout</button>)}
-                <button className="subNavButton" onClick={props.openLoginModal}>Login</button>
                 {localStorage.getItem('bearerToken') && (<button className="subNavButton" onClick={props.openNewAccountModal}>New Account</button>)}
             </div>
     )
