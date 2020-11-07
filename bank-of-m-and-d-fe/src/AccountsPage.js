6b1b74dff32e524128e6a3055ed6c90fa0eb379c
robings <58939401+robings@users.noreply.github.com>
2020-11-07 13:34:27 +0000

    Move login/logout buttons to header component.

--- bank-of-m-and-d-fe/src/AccountsPage.js
+++ bank-of-m-and-d-fe/src/AccountsPage.js
@@ -3,23 +3,15 @@ import Header from './Components/Header/Header';
 import AccountsList from './Components/AccountsList/AccountsList';
 import AccountsNav from './Components/AccountsNav/AccountsNav';
 import AccountsNewForm from './Components/AccountsNewForm/AccountsNewForm';
-import LoginForm from './Components/LoginForm/LoginForm';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import './AccountsPage.css';
 
 function AccountsPage() {
   const [newAccountModalVisibility, setNewAccountModalVisibility] = useState(false);
-  const [loginModalVisibility, setLoginModalVisibility] = useState(false);
 
-  const handleCloseModal = (modal) => {
-    if (modal === "AccountsNewForm") {
-      setNewAccountModalVisibility(false);
-    }
-    if (modal === "LoginForm") {
-      setLoginModalVisibility(false);
-    }
-    
+  const handleCloseModal = () => {
+    setNewAccountModalVisibility(false);
     window.location.reload();
   };
 
@@ -28,19 +20,12 @@ function AccountsPage() {
       <Header />
       <AccountsNav
         openNewAccountModal={() => setNewAccountModalVisibility(true)}
-        openLoginModal={() => setLoginModalVisibility(true)}
       />
       <AccountsList />
       {newAccountModalVisibility && (
         <AccountsNewForm
           newAccountModalVisibility={newAccountModalVisibility}
           closeModal={() => handleCloseModal("AccountsNewForm")}
-        />
-      )}
-      {loginModalVisibility && (
-        <LoginForm
-          loginModalVisibility={loginModalVisibility}
-          closeModal={() => handleCloseModal("LoginForm")}
         />
       )}
       <ToastContainer />
--- bank-of-m-and-d-fe/src/Components/Header/Header.js
+++ bank-of-m-and-d-fe/src/Components/Header/Header.js
@@ -1,14 +1,47 @@
-import React from 'react';
+import React, { useState } from 'react';
+import LoginForm from '../LoginForm/LoginForm';
+import { toast } from 'react-toastify';
 import './header.css';
 import logo from './m-d.jpg';
 
 function Header() {
+    const [loginModalVisibility, setLoginModalVisibility] = useState(false);
+
+    function home() {
+        window.location.reload();
+      }
+
+    const handleLogoutClick = () => {
+        localStorage.removeItem('bearerToken');
+        toast.success("You have logged out.");
+        setTimeout(home, 5000);
+    }
+
+    const handleLoginClick = () => {
+        setLoginModalVisibility(true);
+    }
+
+    const handleCloseModal = () => {
+        setLoginModalVisibility(false);
+        window.location.reload();
+      };
+
     return (
         <header>
             <img src={logo} alt="Fraught Mum and Dad" />
             <h1>
                 Bank Of Mum And Dad
             </h1>
+            <div className="headerButtons">
+            {localStorage.getItem('bearerToken') && (<button className="subNavButton" onClick={handleLogoutClick}>Logout</button>)}
+                <button className="subNavButton" onClick={handleLoginClick}>Login</button>
+            {loginModalVisibility && (
+                <LoginForm
+                loginModalVisibility={loginModalVisibility}
+                closeModal={handleCloseModal}
+                />
+            )}
+          </div>
         </header>
     );
 }
