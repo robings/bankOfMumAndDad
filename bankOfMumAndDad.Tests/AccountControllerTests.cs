8170a09f2470e486bfd4fe131fb2099cf05a217b
robings <58939401+robings@users.noreply.github.com>
2021-05-01 21:10:45 +0100

    Add GetAccounts tests

--- bankOfMumAndDad.Tests/AccountControllerTests.cs
+++ bankOfMumAndDad.Tests/AccountControllerTests.cs
@@ -1,54 +1,184 @@
-﻿
-using bankOfMumAndDad.Source;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Data.Sqlite;
-using NUnit.Framework;
-using System.Data.Common;
-using bankOfMumAndDad.Entities;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Threading.Tasks;
-using System.Diagnostics.CodeAnalysis;
-using System;
+using bankOfMumAndDad.Controllers;
+using bankOfMumAndDad.Entities;
+using bankOfMumAndDad.Responses;
+using Microsoft.AspNetCore.Mvc;
+using NUnit.Framework;
 
 namespace bankOfMumAndDad.Tests
 {
     public class AccountControllerTests
     {
         [Test]
-        public async Task testTest()
+        public async Task GetAccountsGetsAllAccounts()
         {
-            await using var connection = new SqliteConnection("DataSource=:memory");
-            await using var context = BuildDataContext(connection);
+            var factory = new SqliteInMemoryConnectionFactory();
 
-            connection.Open();
-            context.Database.EnsureCreated();
-            var account = new Account
+            var context = factory.CreateSqliteContext();
+
+            var accounts = new List<Account>
             {
-                FirstName = "Cuthbert",
-                LastName = "Dibble",
-                OpeningBalance = 0,
-                CurrentBalance = 150,
-                Deleted = false,
+                new Account
+                {
+                    FirstName = "Cuthbert",
+                    LastName = "Dibble",
+                    OpeningBalance = 0,
+                    CurrentBalance = 150,
+                    Deleted = false,
+                },
+                new Account
+                {
+                    FirstName = "Barney",
+                    LastName = "McGrew",
+                    OpeningBalance = 0,
+                    CurrentBalance = 200,
+                    Deleted = false,
+                },
+                new Account
+                {
+                    FirstName = "Pew",
+                    LastName = "Pew",
+                    OpeningBalance = 0,
+                    CurrentBalance = 300,
+                    Deleted = false,
+                },
             };
 
-            var account2 = new Account
-            {
-                FirstName = "Cuthber",
-                LastName = "Dibble",
-                OpeningBalance = 0,
-                CurrentBalance = 150,
-                Deleted = false,
-            };
-
-            context.Add(account2);
+            context.AddRange(accounts);
             context.SaveChanges();
 
-            var savedAccount = context.Accounts.FirstAsync().Result;
+            var accountController = new AccountController(context);
 
-            Assert.That(savedAccount.FirstName, Is.EqualTo(account.FirstName));
-            context.Dispose();
+            var result = await accountController.GetAccounts();
+            
+            var response = result.Result as OkObjectResult;
+            var responseValue = response.Value as ApiResponse;
+            var responseData = responseValue.Data as List<Account>;
+
+            Assert.That(response.StatusCode, Is.EqualTo(200));
+            Assert.That(responseData.Count, Is.EqualTo(3));
+            Assert.That(responseData, Is.EqualTo(accounts));
         }
 
-        private static DataContext BuildDataContext(DbConnection connection) => new DataContext(new DbContextOptionsBuilder().UseSqlite(connection).Options);
+        [Test]
+        public async Task GetAccountsWhenSomeAccountsAreSetAsDeletedGetsOnlyNonDeletedAccounts()
+        {
+            var factory = new SqliteInMemoryConnectionFactory();
+
+            var context = factory.CreateSqliteContext();
+
+            var accounts = new List<Account>
+            {
+                new Account
+                {
+                    FirstName = "Cuthbert",
+                    LastName = "Dibble",
+                    OpeningBalance = 0,
+                    CurrentBalance = 150,
+                    Deleted = false,
+                },
+                new Account
+                {
+                    FirstName = "Barney",
+                    LastName = "McGrew",
+                    OpeningBalance = 0,
+                    CurrentBalance = 200,
+                    Deleted = true,
+                },
+                new Account
+                {
+                    FirstName = "Pew",
+                    LastName = "Pew",
+                    OpeningBalance = 0,
+                    CurrentBalance = 300,
+                    Deleted = false,
+                },
+            };
+
+            var expectedAccounts = new List<Account>
+            {
+                accounts[0],
+                accounts[2],
+            };
+
+            context.AddRange(accounts);
+            context.SaveChanges();
+
+            var accountController = new AccountController(context);
+
+            var result = await accountController.GetAccounts();
+
+            var response = result.Result as OkObjectResult;
+            var responseValue = response.Value as ApiResponse;
+            var responseData = responseValue.Data as List<Account>;
+
+            Assert.That(response.StatusCode, Is.EqualTo(200));
+            Assert.That(responseData.Count, Is.EqualTo(2));
+            Assert.That(responseData, Is.EqualTo(expectedAccounts));
+        }
+
+        [Test]
+        public async Task GetAccountsWhenThereAreNoAccountsReturnsNotFound()
+        {
+            var factory = new SqliteInMemoryConnectionFactory();
+
+            var context = factory.CreateSqliteContext();
+
+            var accountController = new AccountController(context);
+
+            var result = await accountController.GetAccounts();
+
+            var response = result.Result as NotFoundObjectResult;
+
+            Assert.That(response.StatusCode, Is.EqualTo(404));
+        }
+
+        [Test]
+        public async Task GetAccountsWhenThereAreNoLiveAccountsReturnsNotFound()
+        {
+            var factory = new SqliteInMemoryConnectionFactory();
+
+            var context = factory.CreateSqliteContext();
+
+            var accounts = new List<Account>
+            {
+                new Account
+                {
+                    FirstName = "Cuthbert",
+                    LastName = "Dibble",
+                    OpeningBalance = 0,
+                    CurrentBalance = 150,
+                    Deleted = true,
+                },
+                new Account
+                {
+                    FirstName = "Barney",
+                    LastName = "McGrew",
+                    OpeningBalance = 0,
+                    CurrentBalance = 200,
+                    Deleted = true,
+                },
+                new Account
+                {
+                    FirstName = "Pew",
+                    LastName = "Pew",
+                    OpeningBalance = 0,
+                    CurrentBalance = 300,
+                    Deleted = true,
+                },
+            };
+
+            context.AddRange(accounts);
+            context.SaveChanges();
+
+            var accountController = new AccountController(context);
+
+            var result = await accountController.GetAccounts();
+
+            var response = result.Result as NotFoundObjectResult;
+
+            Assert.That(response.StatusCode, Is.EqualTo(404));
+        }
     }
 }
