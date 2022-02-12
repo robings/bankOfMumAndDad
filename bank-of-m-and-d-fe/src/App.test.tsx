9e471074a9feed842a94b290184ce736537dd627
robings <58939401+robings@users.noreply.github.com>
2022-02-12 19:17:26 +0000

    Fix failing test.

--- bank-of-m-and-d-fe/src/App.test.tsx
+++ bank-of-m-and-d-fe/src/App.test.tsx
@@ -2,41 +2,40 @@ import { act, render, screen } from "@testing-library/react";
 import { MemoryRouter } from "react-router";
 import { toast } from "react-toastify";
 import App from "./App";
+import appStrings from "./constants/app.strings";
 
 describe("app", () => {
-    const renderApp = () => {
-        render(
-            <MemoryRouter>
-                <App />
-            </MemoryRouter>
-        );
-    };
+  const renderApp = () => {
+    render(
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>
+    );
+  };
 
-    test("displays header on starting application", () => {
-      renderApp();
+  test("displays header on starting application", () => {
+    renderApp();
 
-      expect(
-        screen.getByRole("heading", { name: "Bank Of Mum And Dad" })
-      ).toBeInTheDocument();
+    expect(
+      screen.getByRole("heading", { name: appStrings.title })
+    ).toBeInTheDocument();
+  });
+
+  test("displays login on starting application", () => {
+    renderApp();
+
+    expect(screen.getByRole("heading", { name: "Login" })).toBeInTheDocument();
+  });
+
+  test("shows toast if toast requested", async () => {
+    renderApp();
+
+    const theToast = "A toast to you my friend";
+
+    act(() => {
+      toast(theToast);
     });
 
-    test("displays login on starting application", () => {
-      renderApp();
-
-      expect(
-        screen.getByRole("heading", { name: "Login" })
-      ).toBeInTheDocument();
-    });
-
-    test("shows toast if toast requested", async () => {
-        renderApp();
-
-        const theToast = "A toast to you my friend"
-
-        act(() => {
-            toast(theToast);
-        })
-
-        expect(await screen.findByText(theToast)).toBeInTheDocument();
-    });
+    expect(await screen.findByText(theToast)).toBeInTheDocument();
+  });
 });
